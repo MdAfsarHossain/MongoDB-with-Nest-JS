aef75126c4954c53a1bb40e238003a5c0039b54f
MdAfsarHossain <afsar291722@gmail.com>
2025-09-28 19:06:33 +0600

    Project module ts file added

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectController } from './project.controller';
 import { ProjectService } from './project.service';
-import { Developer, DeveloperSchema } from './schemas/developer.schema.';
+import { Developer, DeveloperSchema } from './schemas/developer.schema';
 import { Project, ProjectSchema } from './schemas/project.schema';
 
 @Module({
